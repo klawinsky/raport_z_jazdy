2e2b5c9927883259fceefb28fae7ab07d2f71c48
Pawel <klawinski.pawel@gmail.com>
2025-11-30 13:32:37 +0100

    Update pdf.js

--- js/pdf.js
+++ js/pdf.js
@@ -1,11 +1,12 @@
 // pdf.js
 export function exportPdf(element, filename = 'Raport.pdf') {
   const opt = {
-    margin: 0.5,
+    margin: [10, 10, 10, 10],
     filename,
     image: { type: 'jpeg', quality: 0.98 },
-    html2canvas: { scale: 2, useCORS: true },
-    jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+    html2canvas: { scale: 2, useCORS: true, logging: false },
+    jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+    pagebreak: { mode: ['css', 'legacy'] }
   };
   return html2pdf().set(opt).from(element).save();
 }
