0a3e2468824a193b4fdaea2e4f9a6dd9989312af
Pawel <klawinski.pawel@gmail.com>
2025-12-02 18:54:24 +0100

    Update pdf.js

--- js/pdf.js
+++ js/pdf.js
@@ -1,16 +1,16 @@
 // js/pdf.js
-export async function exportPdf(containerElement, filename = 'raport.pdf') {
-  if (!containerElement) throw new Error('Brak zawartości do eksportu');
+// exportPdf(containerElement, filename)
+export async function exportPdf(container, filename = 'raport.pdf') {
   const opt = {
     margin:       10,
     filename:     filename,
     image:        { type: 'jpeg', quality: 0.98 },
     html2canvas:  { scale: 2, useCORS: true },
-    jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    jsPDF:        { unit: 'pt', format: 'a4', orientation: 'portrait' }
   };
   return new Promise((resolve, reject) => {
     try {
-      html2pdf().set(opt).from(containerElement).save().then(() => resolve(true)).catch(err => reject(err));
+      html2pdf().set(opt).from(container).save().then(() => resolve(true)).catch(err => reject(err));
     } catch (err) { reject(err); }
   });
 }
